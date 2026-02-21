a362e4e3758c55ac40914b736763aedd22c43cf0
PluggeRo <robin@plugge-mail.de>
2026-02-21 13:23:07 +0100

    fix: Fixed renovate config

--- lua/config/plugins/bufferline.lua
+++ lua/config/plugins/bufferline.lua
@@ -1,7 +1,7 @@
 return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "4.9.0",
+  version = "v4.9.0",
   opts = {
     options = {
       mode = "tabs",
